47117280aef9735de75de31951851e44f398d4c0
shadow <lightshadow416@gmail.com>
2025-12-12 15:07:20 +0100

    feat :added pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
+import withPWAInit from "next-pwa";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {},
 };
 
-export default nextConfig;
+// ⚡ next-pwa setup
+const withPWA = withPWAInit({
+  dest: "public", // where the service worker is generated
+  register: true,
+  skipWaiting: true,
+  // disable: process.env.NODE_ENV === "development", // disable in dev mode
+});
+
+// 🧩 Combine both wrappers
+export default withPWA(nextConfig);
